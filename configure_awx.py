b40e1b63f67423ebc6da218c6b2fe8dfaa954dca
ksator <khelil.sator.git@gmail.com>
2018-02-01 01:31:28 +0100

    m

---
+++ configure_awx.py
@@ -0,0 +1,79 @@
+# rest calls to AWX 
+import requests
+from requests.auth import HTTPBasicAuth
+import json
+from pprint import pprint
+
+# this is the default AWX user
+authuser = 'admin'
+authpwd = 'password'
+
+headers = { 'content-type' : 'application/json' }
+url_base = 'http://192.168.233.142'
+
+# create the organization Juniper
+url=url_base + '/api/v2/organizations/'
+payload = {
+    "name": "Juniper"
+}
+rest_call = requests.post(url, headers=headers, auth=(authuser, authpwd), data=json.dumps(payload))
+# pprint (rest_call.json())
+if rest_call.status_code == 201:
+     print 'organization Juniper successfully created'
+else:
+     print 'failed to create the organization Juniper'
+
+# get the id of the organization Juniper
+url = url_base + "/api/v2/organizations/"
+rest_call = requests.get(url, headers=headers, auth=(authuser, authpwd))
+# pprint (rest_call.json())
+for item in rest_call.json()['results']:
+ if item['name'] == 'Juniper':
+  print "Juniper organization is " + str(item['id'])
+
+
+# add a user ksator to the organization Juniper
+url = url_base + '/api/v2/organizations/1/users/'
+payload = {
+    "username": "ksator",
+    "first_name": "khelil",
+    "last_name": "sator",
+    "is_superuser": True,
+    "password": "AWXpassword"
+}
+rest_call = requests.post(url, headers=headers, auth=(authuser, authpwd), data=json.dumps(payload))
+# pprint (rest_call.json())
+if rest_call.status_code == 201:
+     print 'user ksator created and added successfully to the organization Juniper'
+else:
+     print 'failed to create the user ksator in the organization Juniper'
+
+
+"""
+create the user ksator
+payload = {
+    "username": "ksator",
+    "first_name": "khelil",
+    "last_name": "sator",
+    "is_superuser": True,
+    "password": "AWXpassword"
+}
+url=url_base + '/api/v2/users/'
+rest_call = requests.post(url, headers=headers, auth=(authuser, authpwd), data=json.dumps(payload))
+# pprint (rest_call.json())
+if rest_call.status_code == 201:
+     print 'user ksator successfully created'
+else:
+     print 'failed to create the user ksator'
+"""
+
+# get the user id for ksator
+url = url_base + "/api/v2/users/"
+rest_call = requests.get(url, headers=headers, auth=(authuser, authpwd))
+pprint (rest_call.json())
+for item in rest_call.json()['results']:
+ if item['username'] == 'ksator':
+  print "ksator username id is " + str(item['id'])
+
+
+
