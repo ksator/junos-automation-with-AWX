b7c9e65cf6b2d6e944badd3a7d56d0931fd6af97
Khelil Sator <khelil.sator.git@gmail.com>
2018-02-02 09:36:21 +0100

    Update and rename configure_awx_using_your_variables.py to configure_awx.py

--- configure_awx.py
+++ configure_awx.py
@@ -1,5 +1,5 @@
 ###################################################
-# This block indicates the various import
+# This block indicates the various imports
 ###################################################
 import requests
 from requests.auth import HTTPBasicAuth
