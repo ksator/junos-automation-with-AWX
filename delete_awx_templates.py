a8dd71333313c21107d95c5a7c04b9caa842e564
ksator <khelil.sator@gmail.com>
2018-02-02 06:44:20 -0800

    m

--- delete_awx_templates.py
+++ delete_awx_templates.py
@@ -2,19 +2,36 @@
 Description: use this script to make a REST call to AWX in order to delete all AWX templates (i.e to remove all Ansible playbooks from AWX)
 '''
 '''
-Usage: 
-$ python delete_awx_templates.py 
+Usage: $ python delete_awx_templates.py 
 '''
 
 import requests
 from requests.auth import HTTPBasicAuth
+import yaml 
 
+def import_variables_from_file():
+ my_variables_file=open('variables.yml', 'r')
+ my_variables_in_string=my_variables_file.read()
+ # print my_variables_in_string
+ my_variables_in_yaml=yaml.load(my_variables_in_string)
+ # print my_variables_in_yaml
+ # print my_variables_in_yaml['awx']['ip']
+ my_variables_file.close()
+ return my_variables_in_yaml
+
+my_variables_in_yaml=import_variables_from_file()
+
+
+# authuser = my_variables_in_yaml['user']['username']
+# authpwd = my_variables_in_yaml['user']['password']
 authuser = 'admin'
 authpwd = 'password'
-url = 'http://192.168.233.142/api/v2/job_templates'
-my_headers = { 'Accept': 'application/json' }
-rest_call_to_get_all_templates = requests.get(url, auth=HTTPBasicAuth(authuser, authpwd), headers=my_headers)
 
+my_headers = { 'Accept': 'application/json' }
+url = 'http://' + my_variables_in_yaml['awx']['ip'] + '/api/v2/job_templates'
+rest_call_to_get_all_templates = requests.get(url, auth=HTTPBasicAuth(authuser, authpwd), headers=my_headers)
 for item in rest_call_to_get_all_templates.json()['results']:
-    url = 'http://192.168.233.142/api/v2/job_templates/' + str(item["id"])
+    url = 'http://' + my_variables_in_yaml['awx']['ip'] + '/api/v2/job_templates/' + str(item["id"])
     rest_call_to_delete_a_template = requests.delete(url, auth=HTTPBasicAuth(authuser, authpwd))
+
+
